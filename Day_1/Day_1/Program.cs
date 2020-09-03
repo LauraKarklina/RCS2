4c4f53b785acf9c34b0d691f10795205818ef697
Laura <laura.karklina.lk@gmail.com>
2020-09-03 21:15:27 +0300

    Uzdevumi_piemeri
    Ieprikšējo dienu uzdevumi un piemēri pievienoti

---
+++ Day_1/Day_1/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace Day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1();
+            //Task2();
+            //Console.WriteLine("Ieraksti skaitli:");
+            //Console.WriteLine(Task3_3());
+            int sum = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Enter number:");
+                string input = Console.ReadLine();
+                int number = Convert.ToInt32(input);
+
+                sum = sum + number;
+            }
+            Console.WriteLine(sum);
+
+
+        }
+
+        static bool Task3_1()
+        {
+            string input = Console.ReadLine();
+            var number = Convert.ToInt32(input);
+
+            bool positiveOrNegative = number > 0;
+            if (positiveOrNegative)
+            {
+                Console.WriteLine("Skaitlis ir pozitīvs");
+            }
+            else
+            {
+                Console.WriteLine("Skaitlis ir negatīvs");
+            }
+            return positiveOrNegative;
+        }
+        static bool Task3_2()
+        {
+            string input = Console.ReadLine();
+            var number = Convert.ToInt32(input);
+
+            bool positiveOrNegative = number > 0;
+          
+            return positiveOrNegative;
+        }
+        static int Task3_3()
+        {
+            int sum = 0;
+            string input = Console.ReadLine();
+            var numberTimes = Convert.ToInt32(input);
+
+            for (int i = 0; i < numberTimes; i++)
+            {
+                Console.WriteLine("Enter numbers:");
+                string inputNumbers = Console.ReadLine();
+                int numbers = Convert.ToInt32(input);
+
+                sum = sum + numbers;
+            }
+            
+
+            return sum;
+        }
+
+        static void Sample2(int number1, int number2)
+        {
+            Console.WriteLine("Summa ir " + (number1 + number2));
+        }
+
+        static void Sample()
+        {
+
+        }
+        static void Task1()
+        {
+            Console.WriteLine("Enter your number: ");
+
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            if (number > 5)
+
+            {
+                Console.WriteLine("**");
+            }
+
+
+            else
+
+            {
+                Console.WriteLine("*");
+            }
+        }
+        static void Task2()
+        {
+            int sum = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Enter number:");
+                string input = Console.ReadLine();
+                int number = Convert.ToInt32(input);
+
+                sum = sum + number;
+            }
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
---
+++ Uzdevums_3/Uzdevums_3/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Reflection;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1.uzd-Uztaisīt metodi, kas kā parametru paņem vienu int tipa skaitli
+            //un izvada vai skaitlis ir pozitīvs vai negatīvs.
+            Console.WriteLine("Ieraksti skaitli: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Task3_1(number));
+
+
+            //2.uzd-Paņemt kodu no pirmā punkta un pārveidot, 
+            //lai pozitīva skaitļa gadījumā atgriež true, negatīva- false.
+            Console.WriteLine("Ieraksti skaitli: ");
+            var number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Task3_2(number2));
+
+
+            //3.uzd-Uztaisīt metodi, kas kā parametru paņem vienu int tipa skaitli, 
+            //šo skaitli izmantot, lai noteiktu, cik skaitļus cilvēkam jāievada. Izvadīt skaitļu summu. 
+            Console.WriteLine("Ieraksti, cik skaitļus vēlies saskaitīt: ");
+            int skaitluDaudzums = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Skaitļu summa ir: " + Task3_3(skaitluDaudzums));
+
+
+            //4.uzd-Realizēt kāpināšanu ar veseliem/pozitīviem skaitļiem, 
+            //metodei ir vajadzīgi parametri un return. 
+            Console.WriteLine("Ieraksti skaitli, ko vēlies kāpināt: ");
+            var number4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Kādā pakāpē vēlies skaitli: ");
+            var pakape = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Rezultāts ir: " + Task3_4(number4, pakape));
+
+        }
+        static void Task3_1(int number)
+        {
+
+
+            if (number > 0)
+            {
+                Console.WriteLine("Skaitlis ir pozitīvs");
+            }
+            else
+            {
+                Console.WriteLine("Skaitlis ir negatīvs");
+            }
+
+        }
+        static bool Task3_2(int number2)
+        {
+
+            bool positiveOrNegative = number2 > 0;
+
+            return positiveOrNegative;
+        }
+        static int Task3_3(int skaitluDaudzums)
+        {
+
+
+            if (skaitluDaudzums < 1)
+            {
+                Console.WriteLine("Cikls nav iespējams");
+                return;
+            }
+
+            int sum = 0;
+            for (int i = 0; i < skaitluDaudzums; i++)
+            {
+                Console.WriteLine("Ievadi skaitli: ");
+                string input = Console.ReadLine();
+                int number = Convert.ToInt32(input);
+
+                sum += number;
+            }
+
+
+            return sum;
+        }
+        static int Task3_4(int number4, int pakape)
+        {
+
+            int result = 1;
+            for (int i = 0; i < pakape; i++)
+            {
+
+                result = result * number4;
+            }
+
+
+            return result;
+        }
+    }
+}
