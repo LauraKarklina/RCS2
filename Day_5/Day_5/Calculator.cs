50612d290b0a4bc7de889cec3f18d7ab89ff79c1
Laura <laura.karklina.lk@gmail.com>
2020-09-04 20:00:24 +0300

    Task_1
    Unfinished

---
+++ Day_5/Day_5/Calculator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_5
+{
+    class Calculator
+    {
+        static double Multiplication(double number1, double number2)
+        {
+            String sign = Console.ReadLine();
+            double result = 0;
+            if (sign == "*")
+            {
+                result = number1 * number2;
+            }
+
+
+            return result;
+        }
+        static double Division(double number1, double number2)
+        {
+            String sign = Console.ReadLine();
+            double result = 0;
+
+
+            if (sign == "/")
+            {
+                if (number1 > number2)
+                {
+                    result = number1 / number2;
+
+                }
+                else
+                {
+                    result = number2 / number1;
+                }
+
+            }
+
+
+            return result;
+        }
+        static double Sum(double number1, double number2)
+        {
+            String sign = Console.ReadLine();
+            double result = 0;
+            if (sign == "+")
+            {
+                result = number1 + number2;
+            }
+
+            return result;
+        }
+        static double Subtraction(double number1, double number2)
+        {
+            String sign = Console.ReadLine();
+            double result = 0;
+            if (sign == "-")
+            {
+                result = number1 - number2;
+            }
+
+            return result;
+        }
+
+    }
+
+
+
+}
---
+++ Day_5/Day_5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Day_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ievadi pirmo skaitli: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ievadi otro skaitli: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ievadi  +, -, *,/ ");
+            String sign = Console.ReadLine();
+
+            switch (sign)
+            {
+
+                case "*":
+                    Console.WriteLine(Calculator.Multiplication(number1, number2));
+                    break;
+                case "/":
+                    Console.WriteLine(Calculator.Division(number1, number2));
+                    break;
+                case "+":
+                    Console.WriteLine(Calculator.Sum(number1, number2));
+                    break;
+                case "-":
+                    Console.WriteLine(Calculator.Subtraction(number1, number2));
+                    break;
+            }
+
+        }
+    }
+}
