50fb323c1e1c74cd96730a6b16ee2885e2fb1911
LauraK <laura.karklina.lk@gmail.com>
2020-09-05 19:51:54 +0300

    menu izveide

--- Day_5/Day_5/Program.cs
+++ Day_5/Day_5/Program.cs
@@ -6,33 +6,37 @@ namespace Day_5
     {
         static void Main(string[] args)
         {
+            String sign = "";
             //Console.WriteLine("Ievadi pirmo skaitli: ");
             double number1 = InputDouble(1);
 
             //Console.WriteLine("Ievadi otro skaitli: ");
             double number2 = InputDouble(2);
 
-            Console.WriteLine("Ievadi  +, -, *,/ ");
-            String sign = Console.ReadLine();
-
-            switch (sign)
+            while (sign != "exit")
             {
+                Console.WriteLine("Ievadi  +, -, *,/  or exit");
+                sign = Console.ReadLine();
 
-                case "*":
-                    Console.WriteLine(Calculator.Multiplication(number1, number2));
-                    break;
-                case "/":
-                    Console.WriteLine(Calculator.Division(number1, number2));
-                    break;
-                case "+":
-                    Console.WriteLine(Calculator.Sum(number1, number2));
-                    break;
-                case "-":
-                    Console.WriteLine(Calculator.Subtraction(number1, number2));
-                    break;
-                default:
-                    Console.WriteLine("invalid actin");
-                    break;
+                switch (sign)
+                {
+
+                    case "*":
+                        Console.WriteLine(Calculator.Multiplication(number1, number2));
+                        break;
+                    case "/":
+                        Console.WriteLine(Calculator.Division(number1, number2));
+                        break;
+                    case "+":
+                        Console.WriteLine(Calculator.Sum(number1, number2));
+                        break;
+                    case "-":
+                        Console.WriteLine(Calculator.Subtraction(number1, number2));
+                        break;
+                    default:
+                        Console.WriteLine("invalid actin");
+                        break;
+                }
             }
             
             static double InputDouble(double number)
---
+++ Day_5_Lists/Day_5_Lists/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day_5_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> lst = new List<String>();
+            Task2(lst);
+        }
+
+        static void Task2(List<String> lst)
+        {
+            String choice = "";
+
+            while (choice != "exit")
+            {
+                Console.WriteLine("Izvēlies darību-Pievienot, Izvadīt vai exit");
+                choice = Console.ReadLine();
+
+
+
+                switch (choice)
+                {
+                    case "Pievienot":
+                        Console.WriteLine("Cik vērtības pievienot? ");
+                        int vertibuSkaits = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 1; i < vertibuSkaits; i++)
+                        {
+                            Console.WriteLine("Pievieno vērtību nr." + i);
+                            lst.Add(Console.ReadLine());
+                        }
+
+                        break;
+                    case "Izvadit":
+
+                        if (lst.Count == 0)
+                        {
+                            Console.WriteLine("Nav vērtības");
+                        }
+                        foreach (String value in lst)
+                        {
+                            Console.WriteLine(value);
+                        }
+                        break;
+
+                    case "exit":
+                        break;
+                    default:
+                        Console.WriteLine("nepareiza ievade!");
+                        break;
+                }
+
+
+            }
+        }
+
+        static string InputString()
+        {
+            string darbiba = "";
+            try
+            {
+                Console.WriteLine("Ievadiet darbibu");
+                darbiba = Console.ReadLine();
+            }
+            catch
+            {
+                darbiba = InputString();
+            }
+
+            return darbiba;
+        }
+
+    }
+}
