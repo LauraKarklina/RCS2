581efb214a0d2d398edb0c4b1ed088b38f746f8d
LauraK <laura.karklina.lk@gmail.com>
2020-09-10 20:04:21 +0300

    Taisnsturis
    procesā

---
+++ Day_8/Day_8/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Taisnsturis> lstTaisnsturis = new List<Taisnsturis>();
+            AddElement(lstTaisnsturis);
+            PrintLst(lstTaisnsturis);
+        }
+
+        private static void AddElement(List<Taisnsturis> lstTaisnsturis)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Norādi garumu taisnsturim nr." + i);
+                int garums = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Norādi augstumu taisnsturim nr." + i);
+                int augstums = Convert.ToInt32(Console.ReadLine());
+
+
+                lstTaisnsturis.Add(new Taisnsturis(garums, augstums));
+            }
+            
+        }
+        private static void PrintLst(List<Taisnsturis> lstTaisnsturis)
+        {
+            Console.WriteLine();
+
+            if (lstTaisnsturis.Count == 0)
+            {
+                Console.WriteLine("Saraksts ir tukss!");
+                return;
+            }
+
+            Console.WriteLine("Figūru skaits būs te");
+            foreach (Taisnsturis stud in lstTaisnsturis)
+            {
+                stud.PrintInfo();
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Day_8/Day_8/Taisnsturis.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_8
+{
+    class Taisnsturis
+    {
+        private int _garums;
+        public int garums
+        {
+            get
+            {
+                return _garums;
+            }
+            set
+            {
+                if (value >= 1)
+                {
+                    _garums = value;
+                }
+
+                else
+                {
+                    Console.WriteLine("Nederīga vērtība");
+                }
+            }
+        }
+
+        private int _augstums;
+        public int augstums
+        {
+            get
+            {
+                return _augstums;
+            }
+            set
+            {
+                if (value >= 1)
+                {
+                    _augstums = value;
+                }
+                
+                else
+                {
+                    Console.WriteLine("Nederīga vērtība");
+                }
+            }
+        }
+
+        public Taisnsturis(int garums, int augstums)
+        {
+            this.garums = garums;
+            this.augstums = augstums;
+            
+
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine(garums*augstums);
+        }
+    }
+}
