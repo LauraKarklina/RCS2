d263251a2b91db8a5f192eeadff6d3370b75ab3f
LauraK <laura.karklina.lk@gmail.com>
2020-09-11 19:14:23 +0300

    lietotajs ievada faila nosaukumu

---
+++ Day_9/Day_9/FileOperations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Day_9
+{
+    class FileOperations
+    {
+        public static void Read(String name)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(name+"test.txt");
+
+                String line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Neizdevas nolasit failu!");
+            }
+        }
+        public static void Write(String name)
+        {
+
+            try
+            {
+                StreamWriter sw = new StreamWriter(name+"Test.txt", true);
+                sw.WriteLine("some text");
+                
+
+                sw.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Neizdevas ierakstit faila!");
+            }
+        }
+    }
+}
---
+++ Day_9/Day_9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ievadiet faila nosaukumu:");
+            String name = Console.ReadLine();
+
+            FileOperations.Write(name);
+
+            FileOperations.Read(name);
+        }
+    }
+}
