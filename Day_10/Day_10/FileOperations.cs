274c2e6d6b9863b3507cb83ab3c0b34297ca25ff
LauraK <laura.karklina.lk@gmail.com>
2020-09-14 21:33:04 +0300

    nepabeigts
    pamatīgi saputrojos, līdz kamēr sapratu, ka vienkārsi jāsāk risināt uzdevums no jauna ar svaigu galvu

---
+++ Day_10/Day_10/FileOperations.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Day_10
+{
+    class FileOperations
+    {
+        private const String defaultPath = @"C:\Users\Laura IT\Desktop\";
+        private const String filename = "Test.txt";
+        public static List<double> Read()
+        {
+            List<double> lstOfElements = new List<double>();
+            try
+            {
+
+                StreamReader sr = new StreamReader(defaultPath + filename);
+                
+                double line = Convert.ToDouble( sr.ReadLine());
+
+                while (line != 0)
+                {
+                    lstOfElements.Add(line);
+                    line = Convert.ToDouble(sr.ReadLine());
+                }
+
+                sr.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Neizdevas nolasit failu!");
+            }
+
+            return lstOfElements;
+        }
+
+        public static void Write(List<double> lst)
+        {
+
+            try
+            {
+                StreamWriter sw = new StreamWriter(defaultPath + filename);
+
+                foreach (double el in lst)
+                {
+                    sw.WriteLine(el);
+                }
+
+                sw.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Neizdevas ierakstit faila!");
+            }
+        }
+    }
+
+}
---
+++ Day_10/Day_10/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task4();
+        }
+
+        static void Task4()
+        {
+            List<Double> lst = FileOperations.Read();
+
+            String choice = "";
+
+            while (choice != "0")
+            {
+
+                Console.WriteLine("1- Pievienot");
+                Console.WriteLine("3- Dzest");
+                Console.WriteLine("2- Saskaitit");
+                Console.WriteLine("4- Videjas");
+
+
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "2":
+                        Sum(lst);
+                        break;
+                    case "1":
+                        AddElement(lst);
+                        break;
+                    case "3":
+                        RemoveElement(lst);
+                        break;
+                    case "4":
+                        Average(lst);
+                        break;
+                    default:
+                        Console.WriteLine("Nepareiza ievade");
+                        break;
+                }
+            }
+        }
+
+        private static void Sum(List<double> lst)
+        {
+            Console.WriteLine();
+
+
+            double sum = 0;
+            for (int i = 0; i < lst.Count; i++)
+            {
+                sum += lst[i];
+                Console.WriteLine(sum);
+            }
+            Console.WriteLine();
+        }
+        private static void Average(List<double> lst)
+        {
+            Console.WriteLine();
+
+            double sum = 0;
+            double avg = 0;
+            for (int i = 0; i < lst.Count; i++)
+            {
+                sum += lst[i];
+                avg = sum / lst.Count;
+                Console.WriteLine(avg);
+            }
+            Console.WriteLine();
+            FileOperations.Write(lst);
+        }
+        private static void RemoveElement(List<double> lst)
+        {
+            Console.WriteLine();
+
+
+
+            if (lst.Count == 0)
+            {
+                Console.WriteLine("Saraksts ir tukss!");
+                return;
+            }
+
+            try
+            {
+                Console.WriteLine("Kuru elementu velaties dzest?");
+                int toDelete = Convert.ToInt32(Console.ReadLine());
+                lst.RemoveAt(toDelete);
+                FileOperations.Write(lst);
+            }
+            catch
+            {
+                Console.WriteLine("Kludaina ievade!");
+            }
+
+
+            Console.WriteLine();
+        }
+
+        private static void AddElement(List<double> lst)
+        {
+            try
+            {
+                Console.WriteLine();
+                Console.WriteLine("Ievadiet elementu!");
+                lst.Add(Convert.ToDouble(Console.ReadLine()));
+                FileOperations.Write(lst);
+            }
+            catch
+            {
+                Console.WriteLine("invalid");
+            }
+            Console.WriteLine();
+        }
+    }
+}
